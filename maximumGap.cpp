d9f58f64c532f6d634833da592673f5ffcfb3591
frankyzf <frankyzf@gmail.com>
2015-09-06 10:52:19 +0800

    maximum gap

---
+++ maximumGap.cpp
@@ -0,0 +1,18 @@
+//
+// Created by feng on 8/21/15.
+//
+
+#include "common.h"
+
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+
+    }
+};
+
+
+int main(){
+    return 1;
+}
