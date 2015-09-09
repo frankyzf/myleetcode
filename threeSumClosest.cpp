3127d03ba574f0599750f3d147a9ce73c3ba376b
frankyzf <frankyzf@gmail.com>
2015-09-09 11:56:32 +0800

    thress sum closet to remove duplication

--- threeSumClosest.cpp
+++ threeSumClosest.cpp
@@ -1,40 +1,38 @@
 #include "common.h"
+//not checked yet
 class Solution {
 public:
     int threeSumClosest(vector<int> &num, int target) {
         int closest = numeric_limits<int>::max();
-        int res_sum = numeric_limits<int>::max();
         sort(num.begin(), num.end());
         if(num.size() < 3)
             return closest;
-        for(auto it = num.begin(); it != num.end() -2 ; it ++)
-        {
-            auto b = it +1;
-            auto c = num.end() -1;
-            while(b < c)
-            {
-                int dist = abs(*it + *b + *c - target);
-                int sum = *it + *b + *c;
-                //cout << *it << "," << *b << "," << *c << "," << target << ", " << dist << endl;
-                if(dist < closest)
-                {
-                    res_sum = sum;
-                    closest = dist;
+        for(int i = 0; i < num.size(); ++i){
+            if(i > 0 && num[i]  == num[i-1])
+                continue;
+            for(int j = i+1; j < num.size() -1; ++j){
+                if(j > i+1 && num[j] == num[j-1]){
+                    continue;
                 }
-                if(sum < target)
-                    b++;
-                else if (sum > target)
-                    c--;
-                else
-                    return sum;
+                int ps = num[i]+num[j];
+                auto it = lower_bound(num.begin()+j+1, num.end(), target-ps);
+                if (it != num.begin() + j + 1 && abs(ps+*it-target)> abs(ps+*(it-1)-target)) {
+                    -- it;
+                }
+                if(closest == numeric_limits<int>::max()
+                   || abs(ps+*it - target) < abs(closest -target)){
+                    closest = ps + *it;
+                }
             }
         }
-        return res_sum;
+        return closest;
     }
 };
 
 
 int main() {
+    vector<int> data{-1, 2, 1, -4};
+    cout << Solution().threeSumClosest(data, 1);
 
     return 1;
 }
