aff9985864017fed5474ca842547bbd8813db392
frankyzf <frankyzf@gmail.com>
2015-09-14 20:28:55 +0800

    lowest common ancestor

--- lowestCommonAncestorBST.cpp
+++ lowestCommonAncestorBST.cpp
@@ -6,19 +6,44 @@
 
 class Solution {
 public:
-    TreeNode* lowestCommonAncestorBST(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root && p && q) {
-            TreeNode *pl = p->val < q->val? p : q;
-            TreeNode *pr = pl == p? q:p;
-            while (! (root->val>=pl->val && root->val<=pr->val)) {
-                if (root->val > pr->val )
-                    root = root->left;
-                else
-                    root = root->right;
-            }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr)
+            return nullptr;
+        TreeNode* res;
+        function<bool(TreeNode*, TreeNode*)>f= [&f](TreeNode* root, TreeNode* p){
+            if(root == nullptr)
+                return false;
+            if(root == p)
+                return true;
+            else
+                return f(root->left, p) || f(root->right, p);
+        };
+
+        if (p == root){
+            if(f(root, q))
+                return root;
+            else
+                return nullptr;
+        }
+        else if(q == root){
+            if(f(root, p))
+                return root;
+            else
+                return nullptr;
+        }
+        else if(( res = lowestCommonAncestor(root->left, p, q) ) != nullptr){
+            return res;
+        }
+        else if(( res = lowestCommonAncestor(root->right, p, q) ) != nullptr){
+            return res;
+        }
+        else if (f(root->left, p) && f(root->right, q) ){
             return root;
         }
-        return NULL;
+        else if (f(root->left, q) && f(root->right, p)){
+            return root;
+        }
+        return nullptr;
     }
 };
 
