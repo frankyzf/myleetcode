5c78114a48ae271f41870a51a5d7e4cf1d87d40b
frankyzf <frankyzf@gmail.com>
2015-09-19 14:50:40 +0800

    add support for construct binarytree function

--- common.h
+++ common.h
@@ -43,6 +43,37 @@ using namespace std;
         ListNode(int x) : val(x), next(NULL) {}
     };
 
+TreeNode* constructTree(vector<string>& con){
+    TreeNode* root = nullptr;
+    queue<TreeNode*> q;
+    root = new TreeNode(atoi(con[0].c_str()));
+    q.push(root);
+
+    int ci = 1;
+    while(ci < con.size()){
+        int n = q.size();
+        for (int i = 0; i < n; ++i) {
+            TreeNode* pp = q.front(); q.pop();
+            if (con[ci] == "#") {
+                pp->left = nullptr;
+            }
+            else {
+                pp->left = new TreeNode(atoi(s.c_str()));
+                q.push(pp->left);
+            }
+            ++ci;
+
+            if(con[ci] == "#")
+                pp->right = nullptr;
+            else{
+                pp->right = new TreeNode(atoi(s.c_str()));
+                q.push(pp->right);
+            }
+            ++ci;
+        }
+    }
+    return root;
+}
 
 
 #endif //NEW_LEETCODE_COMMON_H
