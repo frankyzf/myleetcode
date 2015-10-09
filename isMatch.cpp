a729560bc47288bb01bf3cc68187ab30a295d67d
frankyzf <frankyzf@gmail.com>
2015-10-09 21:08:47 +0800

    regex

--- isMatch.cpp
+++ isMatch.cpp
@@ -6,23 +6,19 @@ public:
 #ifdef DEBUG
         cout << "string:" << s << " pattern:" << p << endl;
 #endif
-        const int slen = s.length();
-        const int plen = p.length();
-        vector<vector<int>> flags(slen+1, vector<int>(plen+1, -1));
+
         auto remainp = [&s, &p](int pi) {
             while (pi + 1 < p.length() && p[pi + 1] == '*') {
                 pi = pi + 2;
             }
-            return (pi == p.length() || (pi == p.length() - 2 && p[pi + 1] == '*'));
+            return pi == p.length();
         };
-        bool result = false;
 
-        function<bool(int, int)> ff = [&remainp, &s, &p, &ff, &flags, slen, plen, &result](int i, int j) {
+
+        function<bool(int, int)> ff = [&remainp, &s, &p, &ff, &remainp ](int i, int j) {
 #ifdef DEBUG
             cout << " --> i:" << i << " j:" << j << endl;
 #endif
-            if (flags[i][j] != -1)
-                return (bool) flags[i][j];
 
             if (i == s.length())
                 return remainp(j);
@@ -30,61 +26,32 @@ public:
                 return false;
             else {
                 bool bstar = false;
-                if (j < plen - 1 && p[j + 1] == '*')
+                if (j < p.length() - 1 && p[j + 1] == '*')
                     bstar = true;
 
                 if (!bstar) {
                     if (p[j] == '.' || s[i] == p[j]) {
-                        if (i == slen - 1) {
-                            flags[i][j] = remainp(j + 1);
-                            return flags[i][j] == 0 ? false : true;
-                        }
-                        else if (j == plen - 1) {
-                            flags[i][j] = false;
-                            return false;
-                        }
-                        else {
-                            flags[i][j] = ff(i + 1, j + 1);
-                            return flags[i][j] == 0 ? false : true;
-                        }
+                       return ff(i + 1, j + 1);
                     }
                     else {
-                        flags[i][j] = false;
                         return false;
                     }
                 }
                 else {
                     if (p[j] == '.' || s[i] == p[j]) {
-                        if (ff(i, j + 2)) {
-                            flags[i][j] = true;
-                            return true;
-                        }
-                        int k = 1;
-#ifdef DEBUG
-                        cout << "i:" << i << " j:" << j << endl;
-#endif
-                        for (; k <= slen - i; ++k) {
-                            if (p[j] == '.' || s[i + k - 1] == p[j]) {
-                                if (ff(i + k, j + 2)) {
-                                    flags[i][j] = true;
-                                    return true;
-                                }
-                            }
-                            else {
-                                flags[i][j] = false;
-                                return false;
-                            }
-                        }
-
+                        int ii = i;
+                        do {
+                            if(ff(ii, j+2))
+                                return true;
+                            ++ii;
+                        }while(ii < s.length() && (p[j] == '.' || s[ii] == p[j]));
+                        if(ii == s.length())
+                            return remainp(j+2);
+                        else
+                            return ff(ii, j+2);
                     }
                     else {
-                        if (j == plen - 2) {
-                            flags[i][j] = false;
-                            return false;
-                        }
-
-                        flags[i][j] = ff(i, j + 2);
-                        return flags[i][j] == 0 ? false : true;
+                        return  ff(i, j + 2);
                     }
                 }
             }
@@ -97,6 +64,6 @@ public:
 
 
 int main() {
-    cout << boolalpha <<  Solution().isMatch("aa", "a*");
+    cout << boolalpha <<  Solution().isMatch("aa", ".*");
     return 1;
 }
