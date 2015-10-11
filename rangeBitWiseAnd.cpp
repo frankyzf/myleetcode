46477e7e64c8e782d21a712cd50020f7f4dbb7c9
frankyzf <frankyzf@gmail.com>
2015-10-11 11:21:26 +0800

    bit

---
+++ rangeBitWiseAnd.cpp
@@ -0,0 +1,27 @@
+#include "common.h"
+//not checked yet
+/*
+ * Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+For example, given the range [5, 7], you should return 4.
+
+Credits:
+Special thanks to @amrsaqr for adding this problem and creating all test cases.
+
+ */
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int i = 0;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            ++i;
+        }
+        return (m << i);
+    }
+};
+int main() {
+    cout << Solution().rangeBitwiseAnd(0, 2147483647);
+    return 1;
+}
