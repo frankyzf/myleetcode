6749548a78435f0a5cb2e8976746481190b24e85
frankyzf <frankyzf@gmail.com>
2015-11-28 17:50:43 +0800

    state transfer

--- findLadder.cpp
+++ findLadder.cpp
@@ -12,99 +12,75 @@ public:
             return res;
         }
 
-        unordered_multimap<string, string> upstream;
 
-        auto diff = [](const string &str1, const string &str2) {
-            int len1 = str1.length();
-            int len2 = str2.length();
-            int len = min(len1, len2);
-            int cnt = 0;
-            for (int i = 0; i < len; ++i) {
-                if (str1[i] != str2[i])
-                    ++cnt;
-                if (cnt >= 2)
-                    return 2;
+        //find candidates
+        auto findcandidates = [&wordList](const unordered_set<string> &used, string word){
+            vector<string> res;
+            for (int i = 0; i < word.length(); ++i) {
+                char oc = word[i];
+                for (int j = 0; j < 26; ++j) {
+                    if('a'+ j == oc)
+                        continue;
+                    word.replace(i, 1, 1, (char)('a' + j));
+                    if(used.count(word) == 0  && wordList.count(word) ){
+                        res.push_back(word);
+                    }
+                }
+                word.replace(i, 1,1,oc);
             }
-            cnt += max(len1, len2) - len;
-            return cnt;
+            return res;
+        };
+        auto diff = [&endWord](const string& str){
+            int cnt = 0;
+            for (int i = 0; i < str.length(); ++i) {
+                if(str[i] != endWord[i])
+                    cnt ++;
+            }
+            return cnt == 1;
         };
 
-        queue<string> qt;
-        qt.push(beginWord);
-        int len = 0;
-        bool bContinue = true;
-        upstream.insert(make_pair(beginWord, ""));
+        unordered_set<string> visited;
 
-        while (bContinue && !qt.empty()) {
-            ++len;
-            queue<string> tmpq;
-            unordered_multimap<string, string> currentup;
-            while (!qt.empty()) {
-                auto str = qt.front();
-                qt.pop();
-                //cout << "L" << len << " " << str << endl;
-                auto oldstr = str;
-                for (int i = 0; i < str.length(); ++i) {
-                    char och = str[i];
-                    for (int j = 0; j < 26; ++j) {
-                        str[i] = (char) (j + 'a');
-                        if ((wordList.count(str) || str == endWord)&& upstream.count(str) == 0) {
-                            //cout << oldstr << "-->" << str <<endl;
-                            tmpq.push(str);
-                            currentup.insert(make_pair(str, oldstr));
-                            if (str == endWord) {
-                                bContinue = false;
-                            }
-                        }
-                        str[i] = och;
+        function<bool(unordered_map<string, vector<vector<string>>>&)> f =
+                [&f, &wordList, &endWord,findcandidates, &res, &diff, &visited]
+                        (unordered_map<string, vector<vector<string>>>& candidates ){
+                    if(candidates.size() == 0)
+                        return false;
+            unordered_map<string, vector<vector<string>> > mcan;
+            bool bFound = false;
+            for(auto it = candidates.begin(); it != candidates.end(); ++it){
+                visited.insert(it->first);
+                if(diff(it->first) ){
+                    for(auto& path: it->second){
+                        path.push_back(it->first);
+                        res.push_back(move(path));
                     }
+                    bFound = true;
                 }
-            }
-            qt.swap(tmpq);
-            for(auto it = currentup.begin(); it != currentup.end(); ++it){
-                //cout << "upstream:" << it->first << " " << it->second << endl;
-                upstream.insert(make_pair(it->first, it->second));
-            }
-
-        }
-
-        if (!bContinue) {
-            queue<string> qw;
-            qw.push(endWord);
-            res.push_back(vector<string>{endWord});
-            while (!qw.empty()) {
-                string word = qw.front();
-                qw.pop();
-                auto rit = upstream.equal_range(word);
-
-                vector<vector<string>> tt;
-                res.swap(tt);
-
-                bool bFound = false;
-
-                for (int i = 0; i < tt.size(); ++i) {
-                    if (word == beginWord || tt[i].back() != word) {
-                        res.push_back(move(tt[i]));
-                    }
-                    else {
-                        auto b = rit.first;
-                        for (auto b = rit.first; b != rit.second; b++) {
-                            vector<string> tmppath(tt[i]);
-                            tmppath.push_back(b->second);
-                            res.push_back(move(tmppath));
-                            bFound = true;
+                else{
+                    auto cc = findcandidates(visited, it->first);
+                    for(auto& item:cc){
+                        for(auto& path: it->second){
+                            path.push_back(it->first);
+                            mcan[item].push_back(path);
+                            path.pop_back();
                         }
                     }
                 }
-
-                if(bFound){
-                    for(auto b = rit.first; b != rit.second; b++){
-                        qw.push(b->second);
-                    }
-                }
             }
-        }
-        for_each(res.begin(), res.end(), [](vector<string>& path){reverse(path.begin(), path.end());});
+
+            if(bFound)
+                return true;
+            else{
+                return f(mcan);
+            }
+        };
+
+        visited.insert(beginWord);
+        unordered_map<string, vector<vector<string>>> path{{beginWord,{{}}}};
+        f(path);
+        for(auto& item: res)
+            item.push_back(endWord);
         return res;
     }
 };
